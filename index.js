d247548d5ba188ba6a6a3ece481188a61e530ed9
Michiel de Jong <michiel@unhosted.org>
2018-09-20 11:16:20 +0200

    copy from hubbie

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const http = require('http')
+const https = require('https')
+const LE = require('greenlock').LE
+const leChallengeFs = require('le-challenge-fs')
+const leAcmeCore = require('le-acme-core')
+const leStoreBot = require('le-store-certbot')
+
+const LE_ROOT = '~/letsencrypt'
+const HTTP_REDIRECT_PORT = 80
+const HTTPS_PORT = 443
+
+// This function starts a TLS webserver on HTTPS_PORT, with on-the-fly LetsEncrypt cert registration.
+// It also starts a redirect server on HTTP_REDIRECT_PORT, which GreenLock uses for the ACME challenge.
+// Certificates and temporary files are stored in LE_ROOT
+module.exports = function getLetsEncryptServers (domain, handler) {
+  let httpServer
+  const le = LE.create({
+    // server: 'staging',
+    server: 'https://acme-v01.api.letsencrypt.org/directory',
+    acme: leAcmeCore.ACME.create(),
+    store: leStoreBot.create({ configDir: LE_ROOT + '/etc', webrootPath: LE_ROOT + '/var/:hostname' }),
+    challenges: { 'http-01': leChallengeFs.create({ webrootPath: LE_ROOT + '/var/:hostname' }) },
+    agreeToTerms: function (tosUrl, cb) { cb(null, tosUrl) },
+    debug: true
+  })
+  return new Promise((resolve, reject) => {
+    httpServer = http.createServer(le.middleware())
+    httpServer.listen(HTTP_REDIRECT_PORT, (err) => {
+      if (err) { reject(err) } else { resolve() }
+    })
+  }).then(() => {
+    return le.core.certificates.getAsync({
+      email: `letsencrypt+${domain}@gmail.com`,
+      domains: [ domain ]
+    })
+  }).then(function (certs) {
+    if (!certs) {
+      throw new Error('Should have acquired certificate for domains.')
+    }
+    return new Promise((resolve, reject) => {
+      const httpsServer = https.createServer({
+        key: certs.privkey,
+        cert: certs.cert,
+        ca: certs.chain
+      }, handler)
+      httpsServer.listen(HTTPS_PORT, (err) => {
+        if (err) { reject(err) } else { resolve([ httpsServer, httpServer ]) }
+      })
+    })
+  })
+}
