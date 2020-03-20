ca601055818fcb42e18f9a57f63306ee9c28b5e5
Michiel de Jong <michiel@unhosted.org>
2020-03-20 17:04:09 +0100

    not sure why this still not works when I call it from node

--- src/getSelfSignedCert.ts
+++ src/getSelfSignedCert.ts
@@ -4,16 +4,17 @@ import { Pems } from "./getLeCert";
 
 export async function getSelfSignedCert(): Promise<Pems> {
   await new Promise((resolve, reject) => {
-    openssl(
-      "openssl req -nodes -new -x509 -keyout server.key -out server.cert",
-      (err: Error | null, out: Buffer): void => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(out);
-        }
+    const command =
+      "openssl req -nodes -new -x509" +
+      ' -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com"' +
+      " -keyout server.key -out server.cert";
+    openssl(command, (err: Error | null, out: Buffer): void => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(out);
       }
-    );
+    });
   });
   return {
     privkey: readFileSync("server.key").toString(),
